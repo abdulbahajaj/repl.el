7dc4ae181521a7cb2861788675765da53de086e0
abdulbahajaj <abdulbahajaj@gmail.com>
2020-07-29 00:52:39 -0600

    update-image-pin

--- replel-build-info.el
+++ replel-build-info.el
@@ -1,2 +1,2 @@
-(progn (defconst replel--build-info-pined-image-tag "d8f00d85077ad4ebd6181eb60a426eddc9c11cc5"))
+(progn (defconst replel--build-info-pined-image-tag "3101e5220dec22999b877c84e46fbd2d47a11424"))
 (provide 'replel-build-info)
