3472763d26b2bbeef34bd53d9f39e9b51681b465
abdulbahajaj <abdulbahajaj@gmail.com>
2020-07-30 19:17:21 -0600

    Change image pin

--- replel-build-info.el
+++ replel-build-info.el
@@ -1,2 +1,2 @@
-(progn (defconst replel--build-info-pined-image-tag "3101e5220dec22999b877c84e46fbd2d47a11424"))
+(progn (defconst replel--build-info-pined-image-tag "7dc4ae181521a7cb2861788675765da53de086e0"))
 (provide 'replel-build-info)
