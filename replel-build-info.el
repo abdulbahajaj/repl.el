1cc6cd164ae5494f1b50c82679a3c6d7911ee557
abdulbahajaj <abdulbahajaj@gmail.com>
2020-08-01 02:10:58 -0600

    Added default in-repl git user

--- replel-build-info.el
+++ replel-build-info.el
@@ -1,2 +1,2 @@
-(progn (defconst replel--build-info-pined-image-tag "8b92c1b5a5a6310fd45893608b873b89639e3209"))
+(progn (defconst replel--build-info-pined-image-tag "0c358926b37f8902ea40cbd5bb9d7084bf656224"))
 (provide 'replel-build-info)
